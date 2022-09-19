fca9414a9361791cb3e05caf85b68486c73403f6
dechmsee <dmutuimatthews@gmail.com>
2022-09-19 19:04:18 +0000

    Linked list cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -11,7 +11,7 @@ int check_cycle(listint_t *list)
 {
 	listint_t *slow, *fast;
 
-	if (list == NULL && list->next == NULL)
+	if (list == NULL || list->next == NULL)
 		return (0);
 
 	slow = list->next;
