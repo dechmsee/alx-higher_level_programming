69a2fe4b976ed952b43bf09a3c6123fdf7447f2a
dechmsee <dmutuimatthews@gmail.com>
2022-10-01 07:15:45 +0000

    Header File

---
+++ 0x04-python-more_data_structures/Python.h
@@ -0,0 +1,11 @@
+#ifndef _PYTHON_
+#define _PYTHON_
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+
+#endif
