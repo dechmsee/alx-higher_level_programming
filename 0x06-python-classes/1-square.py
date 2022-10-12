66296b308e31727f7661458055e8bf878e297426
Dennis Mutui <dmutuimatthews@gmail.com>
2022-10-12 13:34:19 +0300

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# 0-square.py by Dennis Mutui
+"""Defines a square """
+
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size):
+        """Initialising this square class
+        Args: size - represents the size of the sqaure defined
+        """
+
+        self.__size = size
