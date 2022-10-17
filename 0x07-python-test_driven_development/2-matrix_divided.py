033a07967dd4f28a2d10a8416f57381673125b7e
Dennis Mutui <dmutuimatthews@gmail.com>
2022-10-17 12:49:05 +0300

    README file

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This module defines a matrix dividing function
+"""
+
+
+def matrix_divided(matrix, div)
+
+"""This function divides all elements of a matrix by a given number
+
+    Args:
+        matrix: A list of lists - members can be of type int or float
+        div: Number to be used for the division (can also be a float or integer)
+    Raises:
+        TypeError: If the matrix contains non-numbers
+        TypeError: If the matrix contains rows of different sizes
+    """
---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,6 @@
+#!/usr/bin/py
+"""A class that defines a rectangle"""
+
+class Rectangle:
+    """An empty class that reps a rectangle"""
+    pass
