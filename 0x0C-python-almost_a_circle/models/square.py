ec5c92ac0dd5c41c0aeeceefc8256fd8b01ba4c8
Dennis Mutui <dmutuimatthews@gmail.com>
2022-11-01 21:41:03 +0300

    Changes

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -3,14 +3,15 @@
 
 from models.rectangle import Rectangle
 
+
 class Square(Rectangle):
-    """A square class"""
+    """Represents a square"""
     def __init__(self, size, x=0, y=0, id=None):
         self.size = size
         self.x = x
         self.y = y
-        self.id = id
-        super().__init__(size, x, y, id)
+        self.id = None
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
         """Defines a format for the string representation of the class"""
@@ -18,12 +19,12 @@ class Square(Rectangle):
 
     @property
     def size(self):
-        """Returns the size of the square"""
+        """Gets the value of size"""
         return self.__width
 
     @size.setter
     def size(self, value):
-        """Sets the size of the square"""
+        """Sets the value for size"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value <= 0:
