d0049983f26247c1e800d934468ba12a886f8f58
Dennis Mutui <dmutuimatthews@gmail.com>
2022-11-01 21:39:28 +0300

    And now, the Square!

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -16,3 +16,17 @@ class Square(Rectangle):
         """Defines a format for the string representation of the class"""
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
 
+    @property
+    def size(self):
+        """Returns the size of the square"""
+        return self.__width
+
+    @size.setter
+    def size(self, value):
+        """Sets the size of the square"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+        self.__height = value
