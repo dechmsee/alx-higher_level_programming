534132598b9f62456aa36937176b7ca3b8a3fdd3
dechmsee <dmutuimatthews@gmail.com>
2022-12-19 17:53:20 +0000

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
 const argc = process.argv.length;
 
-if (process.argv.length === 2) {
-  console.log("No argument");
-} else if (process.argv.length === 3) {
-  console.log("Argument found");
+if (argc > 2) {
+  console.log('Argument' + (argc > 3 ? 's' : '') + ' found');
 } else {
-  console.log("Arguments found");
+  console.log('No argument');
 }
-
